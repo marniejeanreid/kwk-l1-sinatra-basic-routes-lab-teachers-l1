f299287c61ee9792e1ae275806426bc5d7adbfd2
Marnie Reid <marniejeanreid@gmail.com>
2018-08-14 15:26:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,13 +1,38 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'Welcome to my page!'
+  end
+  
   get '/name' do
-    "My name is __"
+    'My name is Marnie!'
   end
-  get '/hometown' do
-    "My hometown is __"
-  end
-  get '/favorite-song' do
-    "My favorite song is __"
-  end
+  
 end
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # get '/name' do
+  # # "My name is __"
+  # erb :name
+  # end
+  # get '/hometown' do
+  #   "My hometown is __"
+  # end
+  # get '/favorite-song' do
+  #   "My favorite song is __"
+  # end
+
